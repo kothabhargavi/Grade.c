92df061908d470a955f47bf455e04476af341644
kothabhargavi <68498062+kothabhargavi@users.noreply.github.com>
2020-07-19 18:24:03 +0530

    Grade.c
    To print grades for the marks on the output screen

---
+++ Grade.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\nEnter marks: ",n);
+Scanf("%d",&n);
+if(n>=85 && n<=100)
+{
+printf("\nGRADE A");
+}
+else if(n>=70 && n<=84)
+{
+printf("\nGRADE B");
+}
+else if(n>=55 && n<=69)
+{
+printf("\nGRADE C");
+}
+else if(n>=40 && n<=54)
+{
+Printf("\nGRADE D");
+}
+else
+{
+printf("\nGRADE F");
+}
+}
